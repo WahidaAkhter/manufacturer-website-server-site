363d3c88099804eb46600581f0f99800470f9428
MD Sium <wahidacse280@gmail.com>
2022-05-26 16:33:09 +0600

    registration working

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,16 +1,29 @@
 import React from 'react'
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
+import { useForm } from "react-hook-form";
+
 
 const Login = () => {
+  const [signInWithGoogle, gUser, gLoading, gError] = useSignInWithGoogle(auth);
+
+
+  
   return (
-        <div class="card w-96 bg-base-100 shadow-xl">
-        <div class="card-body">
-          <h2 class="card-title">Card title!</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
-          <div class="card-actions justify-end">
-            <button class="btn btn-primary">Buy Now</button>
-          </div>
-        </div>
-      </div>
+   
+       <div className='flex h-screen justify-center items-center'>
+                <div class="card w-96 bg-base-100 shadow-xl">
+              <div class="card-body">
+                <h2 class="text-center text-2xl font-bold">Login</h2>
+                <div className="divider">OR</div>
+                <button
+                    onClick={() => signInWithGoogle()}
+                    class="btn btn-outline">
+                      Continue With Google
+                </button>
+              </div>
+            </div>
+       </div>
   )
 }
 
