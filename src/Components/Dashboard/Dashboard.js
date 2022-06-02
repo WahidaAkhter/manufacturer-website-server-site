617165d8bc3e69bbdc0b63f6ba8c9b73fea86b63
Sium Wahida Wahida <wahidacse280@gmail.com>
2022-06-02 23:25:34 +0600

    create api

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -18,7 +18,7 @@ const Dashboard = () => {
         <li><Link to="/dashboard">My Items</Link></li>
         <li><Link to="/dashboard/review">My Reviews</Link></li>
         <li><Link to="/dashboard/history">My History</Link></li>
-        { admin && <li><Link to="/dashboard/users">All Users</Link></li>}
+         <li><Link to="/dashboard/users">All Users</Link></li>
       </ul>
     
     </div>
